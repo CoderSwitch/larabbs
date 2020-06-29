37eff33d48a69e7df36ff03579cbe0ae70659cb2
switch <switch_zhou@163.com>
2020-06-29 10:18:40 +0000

    删除评论并删除话题连带评论

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -30,7 +30,8 @@ class RepliesController extends Controller
     {
         $this->authorize('destroy', $reply);
         $reply->delete();
+        return redirect()->route('topics.show', $reply->topic_id)->with('success', '评论删除成功');
 
-        return redirect()->route('replies.index')->with('success', '评论删除成功！');
+//        return redirect()->route('replies.index')->with('success', '评论删除成功！');
     }
 }
