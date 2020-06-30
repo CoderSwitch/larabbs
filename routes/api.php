7886831ef500fb8df37e4573b577fb2f7f6cce1b
switch <switch_zhou@163.com>
2020-06-30 03:12:05 +0000

    发送短信验证码

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Http\Request;
 */
 
 
-Route::prefix('v1')->name('api.v1.')->group(function() {
-    
+Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
+    Route::post('verificationCodes','VerificationCodesController@store')->name('verificationCodes.store');
 });
 
